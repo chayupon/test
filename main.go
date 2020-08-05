b0472b0513a51939bff3795ada603c34c8c5fed6
chayupon <69193376+chayupon@users.noreply.github.com>
2020-08-05 16:33:14 +0700

    Update main.go

--- main.go
+++ main.go
@@ -1,61 +1,25 @@
 package main
 
-import "fmt"
-/*func swap(x  ,y string) (string,string) {
-	return y,x
-}*/
-type Vertex struct
+import ( 
+	"fmt" 
+	"flag"
+)
 func main() {
-
-	fmt.Print("Enter number :")
-	var input float64
-	fmt.Scanf("%f", &input)
-	output := input * 2
-	fmt.Println(output)
-	
-	for number :=1;number <=100;number++ {
-		if number%15 ==0{
-			fmt.Println(number,"FizzBuzz")
-		} else if number%3==0{
-			fmt.Println(number,"Fizz")
-		} else if number%5==0{
-			fmt.Println(number,"Buzz")
-		}else{
-			fmt.Println(number)
-		}
+	flag.Parse()
+	for i := 1; i <= 100; i++ {
+		fizzbuzz(i)	
 	}
-     i :=5 */
-	
-
-   
-   /*switch i :=5;i {
-	case 0:
-		fmt.Println("Zero")
-	case 1:
-		fmt.Println("One")
-	default:
-	    fmt.Println("AAAA")
-
+}
+func fizzbuzz(i int) {
+	fizz := "fizz"
+	buzz := "buzz"
+	if i % 15 == 0 {
+		fmt.Println(i, fizz+buzz)
+	} else if i % 3 == 0 {
+		fmt.Println(i, fizz)
+	} else if i % 5 == 0 {
+		fmt.Println(i, buzz)
+	} else {
+		fmt.Println(i)
 	}
-	result :=true
-	if result ==1{
-		fmt.Println("AAAA")
-	}*/
-
-	
-		//fmt.Println(add(42,13))
-	//	a,b :=swap("hello","World")
-	//	fmt.Println(a,b)
-	/*
-	i,j :=42,2701
-	p :=&i
-	fmt.Println(*p)
-	*p =21
-	fmt.Println(i)
-	p =&j
-	*p =*p/37
-	fmt.Println(j)
-	*/
-
-	
 }

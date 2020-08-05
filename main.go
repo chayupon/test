53bf007e1275e14672a075318c3cecd7972413fa
chayupon <chayupon.kr@tnis.com>
2020-08-05 15:24:36 +0700

    first commit

---
+++ main.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+    "fmt"
+	//"math/rand"
+	//"time"
+    )
+
+func main() {
+/*
+	s1 := rand.NewSource(time.Now().UnixNano())
+    r1 := rand.New(s1)
+	
+	r2 :=r1.Intn(100)
+	fmt.Println("Random Number Is :",r2)
+ 
+	var input int
+	fmt.Scanf("%d", &input)
+	count :=5
+	for i:=0;i<count;i++{
+		fmt.Print("Enter number :")
+		fmt.Scanf("%d", &input)
+		fmt.Println("number :",input)
+		if input >r2 {
+			fmt.Println("มากไป")
+		}else if input <r2{
+			fmt.Println("น้อยไป")
+		}else{
+			fmt.Println("เจอแล้ว")
+		}
+	}
+	*/
+    
+}

b6f90e33651627493615659ec4f5e206f989a0a7
chayupon <69193376+chayupon@users.noreply.github.com>
2020-08-17 16:54:04 +0700

    Update main.go

--- Bot/main.go
+++ Bot/main.go
@@ -1,65 +1,67 @@
 package main
+
 import (
+	"database/sql"
 	"fmt"
 	"net/http"
-	//"log"
-	"database/sql"
+
 	"github.com/gin-gonic/gin"
-	
 	_ "github.com/lib/pq"
-	
 )
 var db *sql.DB
 var err error
 const (
-	host     = "192.168.1.39"
-	port     = 5432 
-	user     = "postgres"
-	password = "Tonkla2639"
-	dbName   = "BotApp"
+	dbHost     = "192.168.1.39"
+	dbUser     = "postgres"
+	dbPassword = "tonkla727426"
+	dbPort     = 5432
+	dbName     = "BotApp"
 )
 
-
-//Member Table
-type Member   struct {
-	Email     string `json:"email"`
+// Member table
+type Member struct {
+	Email    string `json:"Email"`
 	UserID    string `json:"user_id"`
-	UserName  string `json:"username"`
-	Address   string `json:"address"`
+	UserName     string `json:"username"`
+	Address string `json:"address"`
 	
 }
 func main() {
-	dbBot := fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s", host, port, user, password, dbName)
-	db, err := sql.Open("postgres", dbBot)
+    dbsale := fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s", dbHost, dbPort, dbUser, dbPassword, dbName)
+	db, err = sql.Open("postgres", dbsale)
 	if err != nil {
 		fmt.Println("Connect Fail")
+		panic(err)
 	}
+	fmt.Println("Database Connected.")
 	defer db.Close()
+
 	r := gin.Default()
-	r.POST("/register/email",register)
-	r.GET("/member",listMember)
+
+	r.POST("/register/email", register)
+	r.GET("/member", listMember)
 	r.Run()
 }
 func register(c *gin.Context) {
-	sqlStr := "INSERT INTO member VALUES($1,$2,$3,$4)"
-    var member Member
+	sqlStr := "INSERT INTO member VALUES($1, $2, $3, $4)"
+
+	var member Member
 	err := c.BindJSON(&member)
 	if err != nil {
 		fmt.Println(err)
 	}
 	fmt.Println(member)
-	_, err = db.Exec(sqlStr, member.Email, member.UserID, member.UserName,member.Address)
+	_, err = db.Exec(sqlStr, member.Email,member.UserID,member.UserName,member.Address)
 	if err != nil {
 		fmt.Println(err)
 	}
 }
 func listMember(c *gin.Context) {
-	sqlStr := "select username FROM member"
+	sqlStr := "SELECT username  FROM member"
 	rows, err := db.Query(sqlStr)
 	if err != nil {
-		fmt.Println("failed")
-		fmt.Println(err)
-	}
+		fmt.Println("FAIL")
+		}
 	for rows.Next() {
 		var username string
 		if err := rows.Scan(&username); err != nil {
@@ -73,4 +75,3 @@ func listMember(c *gin.Context) {
 		fmt.Println(rows.Err())
 	}
 }
-

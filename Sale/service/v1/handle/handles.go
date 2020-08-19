d1b46a3c8d1dad5fe46381ae3de9cb629b80f332
chayupon <69193376+chayupon@users.noreply.github.com>
2020-08-19 11:19:59 +0700

    Update handles.go

--- Sale/service/v1/handle/handles.go
+++ Sale/service/v1/handle/handles.go
@@ -1,65 +1,18 @@
-package register
+package handle
 
 import (
 	"database/sql"
-	"fmt"
-	"net/http"
-
+	
+	"github.com/chayupon/test/Sale/service/v1/register"
 	"github.com/gin-gonic/gin"
-	
 )
-//Query input to db
-type Query struct{
-	db *sql.DB
-}
-// Member table
-type Member struct {
-	Email     string `json:"Email"`
-	UserID    string `json:"user_id"`
-	UserName  string `json:"username"`
-	Address   string `json:"address"`
-	
-}
-//register
-func (input Query) register(c *gin.Context) {
-	sqlStr := "INSERT INTO member VALUES($1, $2, $3, $4)"
-    var member Member
-	err := c.BindJSON(&member)
-	if err != nil {
-		fmt.Println(err)
-		c.String(http.StatusBadRequest, err.Error())
-		return 
+// Router app
+func Router(db *sql.DB) {
+	regist :=register.Input{
+		Db : db,
 	}
-	fmt.Printf("%+v",member)
-	_, err = input.db.Exec(sqlStr, member.Email,member.UserID,member.UserName,member.Address)
-	if err != nil {
-		fmt.Println(err)
-		c.String(http.StatusBadRequest, err.Error())
-		return
-	}
-	c.JSON(http.StatusOK, gin.H{
-		"status" :"OK",
-	})
+	r := gin.Default()
+	r.POST("/register/email", regist.Email)
+	r.GET("/member", regist.ListAccounts)
+	r.Run()
 }
-//Get (Select database)
-func (input Query) listMember(c *gin.Context) {
-	sqlStr := `SELECT "UserName" FROM member`
-	rows, err := input.db.Query(sqlStr)
-	if err != nil {
-		fmt.Println("FAIL")
-		c.String(http.StatusBadRequest, err.Error())
-		return
-		}
-    defer rows.Close()
-	for rows.Next() {
-		var username string
-		if err := rows.Scan(&username); err != nil {
-			fmt.Println(err)
-		}
-		c.JSON(http.StatusOK, gin.H{
-			"username": username,
-		})
-	
-    }
-}
-
